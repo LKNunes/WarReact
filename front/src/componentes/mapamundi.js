bdd7657156bba2d2c1227faa389f62b75b78e5ab
Lucas Nunes <lucas.gomes.g@gmail.com>
2025-07-04 16:37:50 -0300

    Ajustes de SVG Mundi Front

--- front/src/componentes/mapamundi.js
+++ front/src/componentes/mapamundi.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { ReactComponent as WorldMap } from '../assets/Mundi3.svg';
+//import { ReactComponent as WorldMapMar } from '../assets/Mundi3Mar.svg';
+
+
 import './mapamundi.css';
 
 export default function MapaMundi() {
-  const handleTerritoryClick = (id) => {
+  const handleTerritoryClick = (id,name) => {
     console.log('Território clicado:', id);
+        console.log('Nome:',name);
+
   };
 
   return (
-    <div className="absolute inset-2 flex items-center justify-center overflow-hidden">
-      <WorldMap
-        onClick={(e) => {
-          if (e.target.id) handleTerritoryClick(e.target.id);
-        }}
-        className="w-full h-full hoverable-map"
-      />
-    </div>
+<div className="absolute inset-2 flex items-center justify-center overflow-hidden">
+  
+  {//  <div className="absolute inset-0 z-0 w-full h-full overflow-hidden">
+     // <WorldMapMar className="w-full h-full" />
+    //</div>
+    }
+
+  <div className="absolute inset-0 z-10 w-full h-full">
+    <WorldMap
+      onClick={(e) => {
+        if (e.target.id) handleTerritoryClick(e.target.id, e.target.getAttribute('data-name'));
+      }}
+      className="w-full h-full hoverable-map"
+    />
+  </div>
+</div>
+
+
   );
 }
